08915229987710ad2a8f71b8bea188acd8269cc7
sobayashi <sophieobayashi@gmail.com>
2018-06-09 14:12:38 -0700

    changed config path and commented out routes require

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
-require("./routes/html-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+// require("./routes/author-api-routes.js")(app);
+// require("./routes/post-api-routes.js")(app);
 
 db.sequelize.sync({}).then(function() {
     app.listen(PORT, function() {
