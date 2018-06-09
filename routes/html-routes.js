2980daccb1d1611f17d447ded47aa49a8198df38
Jesse <laethaka@gmail.com>
2018-06-09 15:12:44 -0700

    facebook login works

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,6 +5,10 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-  
+
+    app.get("/landing", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/landing.html"));
+    });
+
   };
   
