a181b8efae7d5e3f383b5a1f0284272cd37e527c
M Jesús Otero <mjesuotero@airbears2-10-142-146-30.airbears2.1918.berkeley.edu>
2018-06-12 14:47:25 -0700

    dashboard

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,31 +5,33 @@ module.exports = function(app) {
   // use res.render to load up an ejs view file
   // Login/Index Page
   app.get("/", function(req, res) {
-    res.render('pages/index');
+    res.render('../views/pages/index');
   });
 
-  app.get("/family/:nickname", function(req, res) {
-    var famName = req.params.nickname;
-    console.log("famName " + famName);
-    res.render('pages/landing',{
-      famName: famName
-    });
+  app.get("/landing", function(req, res) {
+    res.render('../views/pages/landing');
   });
+
+  // app.get("/family/:nickname", function(req, res) {
+  //   res.render('pages/landing'
+  //   , {
+  //     famName: req.params.nickname
+  //   });
+  // });
   
   // Family Dashboard Page 
   app.get('/dashboard', function(req, res) {
-    res.render('/pages/dashboard');
+    res.render('../views/pages/dashboard');
   });
 
   // Registration Page
   app.get('/register', function(req, res) {
-    res.render('pages/register');
+    res.render('../views/pages/register');
   });
 
   // Landing Page 
   app.get('/landing', function(req, res) {
-    res.render('pages/landing');
+    res.render('../views/pages/landing');
   });
 
 };
-
