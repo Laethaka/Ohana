0cdf39b39a7f8fcf751be3eb1537475daa13cb3e
Jesse <laethaka@gmail.com>
2018-06-12 10:12:42 -0700

    pulling morning

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-
 var app = express();
 var PORT = process.env.PORT || 3344;
 
@@ -9,11 +8,6 @@ var db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// app.engine('ejs', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
-app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
