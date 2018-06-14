f7b2a644173f190d199abbdf36ea45270607fb77
M Jesús Otero <mjesuotero@airbears2-10-142-164-50.airbears2.1918.berkeley.edu>
2018-06-14 11:56:55 -0700

    fix login button

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 -- DROP DATABASE IF EXISTS ohana_db;
--- CREATE DATABASE ohana_db;
+CREATE DATABASE ohana_db;
 
 USE ohana_db;
 -- delete from Users;
 -- insert into occasions values
 
-select * from occasions;
+-- select * from occasions;
 
 -- select * from Users;
 
